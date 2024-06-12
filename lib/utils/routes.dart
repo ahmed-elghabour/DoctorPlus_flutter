48d0024cf2db2b67afb0180ee802a0acb9fdb7b4
ARahmaan96 <a.rahman.khallaf@gmail.com>
2024-06-13 02:26:56 +0300

    add Missing Sections to README.md

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,4 +2,7 @@ class Routes {
   static const String home = "/";
   static const String login = "auth/login";
   static const String register = "auth/sign-up";
+  // A.Rahman
+  static const String profile = "user/profile";
+  // end
 }
