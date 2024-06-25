9577e0a06a622b5c7222935a5b25ad628475f6fd
Amr Yehia <amr.yehiaz100@gmail.com>
2024-06-26 00:56:05 +0300

    dad

---
+++ lib/presentation/Public Profile/pages/public_profile.dart
@@ -0,0 +1,130 @@
+import 'package:doctor_plus/data/model/appointment.dart';
+import 'package:doctor_plus/presentation/profile/profile.dart';
+import 'package:doctor_plus/utils/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+class PublicProfile extends StatelessWidget {
+  const PublicProfile({super.key});
+  @override
+  Widget build(BuildContext context) {
+  final Map<String,dynamic> patient = ModalRoute.of(context)!.settings.arguments as Map<String,dynamic> ;
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 36, 124, 255),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text(patient['patientName']),
+      ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                const SizedBox(height: 60),
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        const SizedBox(height: 90),
+                        NameWidget(
+                          name:
+                              SharedPreference().getString(key: 'user.name') ??
+                                  'No Name',
+                        ),
+                        EmailWidget(
+                          email:
+                              SharedPreference().getString(key: 'user.email') ??
+                                  'No email',
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 19, horizontal: 20),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: const Color.fromARGB(
+                                            255, 234, 242, 255),
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    padding: const EdgeInsets.all(15),
+                                    width: 60,
+                                    child: Image.asset("assets/icons/id.png"),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  const Text(
+                                    "Personal Information",
+                                    style: TextStyle(
+                                        fontSize: 19,
+                                        fontWeight: FontWeight.w400),
+                                  )
+                                ],
+                              ),
+                              const Divider(
+                                height: 20,
+                                thickness: 1,
+                                color: Colors.black12,
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: const Color.fromARGB(
+                                            255, 255, 238, 239),
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    padding: const EdgeInsets.all(15),
+                                    width: 60,
+                                    child:
+                                        Image.asset("assets/icons/payment.png"),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  const Text(
+                                    "Payment",
+                                    style: TextStyle(
+                                        fontSize: 19,
+                                        fontWeight: FontWeight.w400),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Positioned.fill(
+                top: 60,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: ProfileImage(
+                    imageUrl: SharedPreference().getString(key: 'user.image'),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
