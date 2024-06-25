3fb93ee0386a70a3a941416079f93c9b6d67eb89
ARahmaan96 <a.rahman.khallaf@gmail.com>
2024-06-25 14:58:43 +0300

    update bookAppointment

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,7 @@ class MyApp extends StatelessWidget {
               Routes.specialization: (context) =>
                   const SpecializationPage(specialization: ''),
             },
+            // initialRoute: Routes.bookAppointment,
             initialRoute: Routes.home,
           );
         }),
