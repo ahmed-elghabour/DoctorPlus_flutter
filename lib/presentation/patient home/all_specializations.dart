9e4085434264a1da3fa8e638db57095e3a8d01e6
AhmedAmgad28 <ahmed.amgad282001@yahoo.com>
2024-06-25 23:34:20 +0300

    change app bar in specializations page

--- lib/presentation/patient home/all_specializations.dart
+++ lib/presentation/patient home/all_specializations.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:doctor_plus/core/widgets/card_specialization.dart';
 import 'package:doctor_plus/data/demo.dart';
+import 'package:doctor_plus/core/widgets/custom_app_bar.dart';
 import 'package:doctor_plus/presentation/specialization/specialization_page.dart';
 
 class AllSpecializationsPage extends StatefulWidget {
@@ -33,11 +34,10 @@ class _AllSpecializationsPageState extends State<AllSpecializationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('All Specializations'),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(56),
-          child: Padding(
+      appBar: const MyCustomAppBar(title: 'All Specializations'),
+      body: Column(
+        children: [
+          Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               controller: searchController,
@@ -54,37 +54,41 @@ class _AllSpecializationsPageState extends State<AllSpecializationsPage> {
               ),
             ),
           ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: filteredSpecializations.isEmpty
-            ? const Center(child: Text('No results found'))
-            : GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  childAspectRatio: 3 / 2,
-                ),
-                itemCount: filteredSpecializations.length,
-                itemBuilder: (context, index) {
-                  final specialization = filteredSpecializations[index];
-                  return SpecializationCard(
-                    title: specialization,
-                    imagePath: 'assets/specializations/$specialization.png',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SpecializationPage(
-                              specialization: specialization),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: filteredSpecializations.isEmpty
+                  ? const Center(child: Text('No results found'))
+                  : GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        childAspectRatio: 3 / 2,
+                      ),
+                      itemCount: filteredSpecializations.length,
+                      itemBuilder: (context, index) {
+                        final specialization = filteredSpecializations[index];
+                        return SpecializationCard(
+                          title: specialization,
+                          imagePath:
+                              'assets/specializations/$specialization.png',
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SpecializationPage(
+                                    specialization: specialization),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+            ),
+          ),
+        ],
       ),
     );
   }
