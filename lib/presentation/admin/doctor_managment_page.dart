98a5c7823fc8823f601c451960d4fce3a721e9c1
AhmedAmgad28 <ahmed.amgad282001@yahoo.com>
2024-06-26 10:41:49 +0300

    custom app bar in admin pages

--- lib/presentation/admin/doctor_managment_page.dart
+++ lib/presentation/admin/doctor_managment_page.dart
@@ -1,13 +1,14 @@
+import 'package:doctor_plus/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class DoctorManagmentPage extends StatelessWidget {
-  static const routeName = '/newDoctorsPage';
+  //static const routeName = '/newDoctorsPage';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Doctor Managment')),
-      body: Center(child: const Text('Doctor Managment Page')),
+    return const Scaffold(
+      appBar: MyCustomAppBar(title: 'Doctor Managment'),
+      body: Center(child: Text('Doctor Managment Page')),
     );
   }
 }
