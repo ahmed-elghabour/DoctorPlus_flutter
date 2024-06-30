a9238876cacfbc99887db9e47c54d8cfc555b6c9
AhmedAmgad28 <ahmed.amgad282001@yahoo.com>
2024-06-30 23:21:15 +0300

    first graph for trial

--- lib/presentation/admin/statistics_page.dart
+++ lib/presentation/admin/statistics_page.dart
@@ -1,6 +1,8 @@
 import 'package:doctor_plus/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class StatisticsPage extends StatelessWidget {
   static const routeName = '/statisticsPage';
@@ -20,6 +22,18 @@ class StatisticsPage extends StatelessWidget {
         await _calculateMostAppointmentsDoctor(doctorsSnapshot.docs);
     final highestRatedDoctor =
         await _calculateHighestRatedDoctor(doctorsSnapshot.docs);
+    final appointmentsToday =
+        await _calculateAppointmentsToday(doctorsSnapshot.docs);
+    final appointmentsLastWeek =
+        await _calculateAppointmentsLastWeek(doctorsSnapshot.docs);
+    final appointmentsLastMonth =
+        await _calculateAppointmentsLastMonth(doctorsSnapshot.docs);
+    final mostAppointmentsDoctorThisWeek =
+        await _calculateMostAppointmentsDoctorThisWeek(doctorsSnapshot.docs);
+    final mostAppointmentsDoctorThisMonth =
+        await _calculateMostAppointmentsDoctorThisMonth(doctorsSnapshot.docs);
+    final topRatedDoctorLastMonth =
+        await _calculateTopRatedDoctorLastMonth(doctorsSnapshot.docs);
 
     return {
       'totalDoctors': doctorsSnapshot.docs.length,
@@ -27,6 +41,12 @@ class StatisticsPage extends StatelessWidget {
       'appointmentsPerDay': appointmentsPerDay,
       'mostAppointmentsDoctor': mostAppointmentsDoctor,
       'highestRatedDoctor': highestRatedDoctor,
+      'appointmentsToday': appointmentsToday,
+      'appointmentsLastWeek': appointmentsLastWeek,
+      'appointmentsLastMonth': appointmentsLastMonth,
+      'mostAppointmentsDoctorThisWeek': mostAppointmentsDoctorThisWeek,
+      'mostAppointmentsDoctorThisMonth': mostAppointmentsDoctorThisMonth,
+      'topRatedDoctorLastMonth': topRatedDoctorLastMonth,
     };
   }
 
@@ -60,7 +80,22 @@ class StatisticsPage extends StatelessWidget {
                 ),
                 _buildStatisticsCard(
                   title: 'Total Appointments',
-                  count: data['appointmentsPerDay']?.values.fold(0, (a, b) => a + b) ?? 0,
+                  count: data['appointmentsPerDay']
+                          ?.values
+                          .fold(0, (a, b) => a + b) ??
+                      0,
+                ),
+                _buildStatisticsCard(
+                  title: 'Appointments Today',
+                  count: data['appointmentsToday'] ?? 0,
+                ),
+                _buildStatisticsCard(
+                  title: 'Appointments Last Week',
+                  count: data['appointmentsLastWeek'] ?? 0,
+                ),
+                _buildStatisticsCard(
+                  title: 'Appointments Last Month',
+                  count: data['appointmentsLastMonth'] ?? 0,
                 ),
                 _buildStatisticsCard(
                   title: 'Doctor with Most Appointments',
@@ -68,10 +103,29 @@ class StatisticsPage extends StatelessWidget {
                   subtitle: data['mostAppointmentsDoctor']?['doctorName'],
                 ),
                 _buildStatisticsCard(
+                  title: 'Doctor with Most Appointments This Week',
+                  count: data['mostAppointmentsDoctorThisWeek']?['count'] ?? 0,
+                  subtitle: data['mostAppointmentsDoctorThisWeek']
+                      ?['doctorName'],
+                ),
+                _buildStatisticsCard(
+                  title: 'Doctor with Most Appointments This Month',
+                  count: data['mostAppointmentsDoctorThisMonth']?['count'] ?? 0,
+                  subtitle: data['mostAppointmentsDoctorThisMonth']
+                      ?['doctorName'],
+                ),
+                _buildStatisticsCard(
                   title: 'Highest Rated Doctor',
                   count: data['highestRatedDoctor']?['rating']?.toInt() ?? 0,
                   subtitle: data['highestRatedDoctor']?['doctorName'],
                 ),
+                _buildStatisticsCard(
+                  title: 'Top Rated Doctor Last Month',
+                  count:
+                      data['topRatedDoctorLastMonth']?['rating']?.toInt() ?? 0,
+                  subtitle: data['topRatedDoctorLastMonth']?['doctorName'],
+                ),
+                _buildAppointmentsPerDayChart(data['appointmentsPerDay'] ?? {}),
               ],
             ),
           );
@@ -104,10 +158,7 @@ class StatisticsPage extends StatelessWidget {
           .get();
 
       for (var appointment in appointmentsSnapshot.docs) {
-        final date = appointment['date']
-            .toString()
-            .split('T')
-            .first;
+        final date = appointment['date'].toString().split('T').first;
         appointmentsPerDay[date] = (appointmentsPerDay[date] ?? 0) + 1;
       }
     }
@@ -169,7 +220,8 @@ class StatisticsPage extends StatelessWidget {
 
       for (var review in reviewsSnapshot.docs) {
         final rating = review['rate'] as double;
-        doctorRatings[doctor.id] = (doctorRatings[doctor.id] ?? [])..add(rating);
+        doctorRatings[doctor.id] = (doctorRatings[doctor.id] ?? [])
+          ..add(rating);
       }
     }
 
@@ -207,11 +259,297 @@ class StatisticsPage extends StatelessWidget {
       'rating': highestAvgRating
     };
   }
-}
 
-class TimeSeriesSales {
-  final DateTime time;
-  final int sales;
+  Future<int> _calculateAppointmentsToday(
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> doctors) async {
+    final now = DateTime.now();
+    final today = DateFormat('yyyy-MM-dd').format(now);
+    int appointmentsToday = 0;
 
-  TimeSeriesSales(this.time, this.sales);
+    for (var doctor in doctors) {
+      final appointmentsSnapshot = await FirebaseFirestore.instance
+          .collection('doctors')
+          .doc(doctor.id)
+          .collection('appointments')
+          .where('date', isEqualTo: today)
+          .get();
+
+      appointmentsToday += appointmentsSnapshot.docs.length;
+    }
+
+    return appointmentsToday;
+  }
+
+  Future<int> _calculateAppointmentsLastWeek(
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> doctors) async {
+    final now = DateTime.now();
+    final lastWeek = now.subtract(const Duration(days: 7));
+    int appointmentsLastWeek = 0;
+
+    for (var doctor in doctors) {
+      final appointmentsSnapshot = await FirebaseFirestore.instance
+          .collection('doctors')
+          .doc(doctor.id)
+          .collection('appointments')
+          .where('date',
+              isGreaterThanOrEqualTo: DateFormat('yyyy-MM-dd').format(lastWeek))
+          .get();
+
+      appointmentsLastWeek += appointmentsSnapshot.docs.length;
+    }
+
+    return appointmentsLastWeek;
+  }
+
+  Future<int> _calculateAppointmentsLastMonth(
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> doctors) async {
+    final now = DateTime.now();
+    final lastMonth = now.subtract(const Duration(days: 30));
+    int appointmentsLastMonth = 0;
+
+    for (var doctor in doctors) {
+      final appointmentsSnapshot = await FirebaseFirestore.instance
+          .collection('doctors')
+          .doc(doctor.id)
+          .collection('appointments')
+          .where('date',
+              isGreaterThanOrEqualTo:
+                  DateFormat('yyyy-MM-dd').format(lastMonth))
+          .get();
+
+      appointmentsLastMonth += appointmentsSnapshot.docs.length;
+    }
+
+    return appointmentsLastMonth;
+  }
+
+  Future<Map<String, dynamic>> _calculateMostAppointmentsDoctorThisWeek(
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> doctors) async {
+    final now = DateTime.now();
+    final startOfWeek = now.subtract(Duration(days: now.weekday - 1));
+    final Map<String, int> doctorAppointmentsCount = {};
+
+    for (var doctor in doctors) {
+      final appointmentsSnapshot = await FirebaseFirestore.instance
+          .collection('doctors')
+          .doc(doctor.id)
+          .collection('appointments')
+          .where('date',
+              isGreaterThanOrEqualTo:
+                  DateFormat('yyyy-MM-dd').format(startOfWeek))
+          .get();
+
+      doctorAppointmentsCount[doctor.id] = appointmentsSnapshot.docs.length;
+    }
+
+    if (doctorAppointmentsCount.isEmpty) {
+      return {
+        'doctorId': 'N/A',
+        'doctorName': 'N/A',
+        'count': 0,
+      };
+    }
+
+    final mostAppointmentsDoctorId = doctorAppointmentsCount.keys.reduce(
+        (a, b) =>
+            doctorAppointmentsCount[a]! > doctorAppointmentsCount[b]! ? a : b);
+
+    final doctorSnapshot = await FirebaseFirestore.instance
+        .collection('doctors')
+        .doc(mostAppointmentsDoctorId)
+        .get();
+    final doctorData = doctorSnapshot.data();
+    final doctorName =
+        'Dr. ${doctorData?['fName'] ?? 'Unknown'} ${doctorData?['lName'] ?? ''}';
+
+    return {
+      'doctorId': mostAppointmentsDoctorId,
+      'doctorName': doctorName,
+      'count': doctorAppointmentsCount[mostAppointmentsDoctorId]
+    };
+  }
+
+  Future<Map<String, dynamic>> _calculateMostAppointmentsDoctorThisMonth(
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> doctors) async {
+    final now = DateTime.now();
+    final startOfMonth = DateTime(now.year, now.month, 1);
+    final Map<String, int> doctorAppointmentsCount = {};
+
+    for (var doctor in doctors) {
+      final appointmentsSnapshot = await FirebaseFirestore.instance
+          .collection('doctors')
+          .doc(doctor.id)
+          .collection('appointments')
+          .where('date',
+              isGreaterThanOrEqualTo:
+                  DateFormat('yyyy-MM-dd').format(startOfMonth))
+          .get();
+
+      doctorAppointmentsCount[doctor.id] = appointmentsSnapshot.docs.length;
+    }
+
+    if (doctorAppointmentsCount.isEmpty) {
+      return {
+        'doctorId': 'N/A',
+        'doctorName': 'N/A',
+        'count': 0,
+      };
+    }
+
+    final mostAppointmentsDoctorId = doctorAppointmentsCount.keys.reduce(
+        (a, b) =>
+            doctorAppointmentsCount[a]! > doctorAppointmentsCount[b]! ? a : b);
+
+    final doctorSnapshot = await FirebaseFirestore.instance
+        .collection('doctors')
+        .doc(mostAppointmentsDoctorId)
+        .get();
+    final doctorData = doctorSnapshot.data();
+    final doctorName =
+        'Dr. ${doctorData?['fName'] ?? 'Unknown'} ${doctorData?['lName'] ?? ''}';
+
+    return {
+      'doctorId': mostAppointmentsDoctorId,
+      'doctorName': doctorName,
+      'count': doctorAppointmentsCount[mostAppointmentsDoctorId]
+    };
+  }
+
+  Future<Map<String, dynamic>> _calculateTopRatedDoctorLastMonth(
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> doctors) async {
+    final now = DateTime.now();
+    final lastMonth = now.subtract(const Duration(days: 30));
+    final Map<String, List<double>> doctorRatings = {};
+
+    for (var doctor in doctors) {
+      final reviewsSnapshot = await FirebaseFirestore.instance
+          .collection('doctors')
+          .doc(doctor.id)
+          .collection('reviews')
+          .where('date',
+              isGreaterThanOrEqualTo:
+                  DateFormat('yyyy-MM-dd').format(lastMonth))
+          .get();
+
+      for (var review in reviewsSnapshot.docs) {
+        final rating = review['rate'] as double;
+        doctorRatings[doctor.id] = (doctorRatings[doctor.id] ?? [])
+          ..add(rating);
+      }
+    }
+
+    if (doctorRatings.isEmpty) {
+      return {
+        'doctorId': 'N/A',
+        'doctorName': 'N/A',
+        'rating': 0.0,
+      };
+    }
+
+    final highestRatedDoctorId = doctorRatings.keys.reduce((a, b) {
+      final avgRatingA =
+          doctorRatings[a]!.reduce((a, b) => a + b) / doctorRatings[a]!.length;
+      final avgRatingB =
+          doctorRatings[b]!.reduce((a, b) => a + b) / doctorRatings[b]!.length;
+      return avgRatingA > avgRatingB ? a : b;
+    });
+
+    final highestAvgRating =
+        doctorRatings[highestRatedDoctorId]!.reduce((a, b) => a + b) /
+            doctorRatings[highestRatedDoctorId]!.length;
+
+    final doctorSnapshot = await FirebaseFirestore.instance
+        .collection('doctors')
+        .doc(highestRatedDoctorId)
+        .get();
+    final doctorData = doctorSnapshot.data();
+    final doctorName =
+        'Dr. ${doctorData?['fName'] ?? 'Unknown'} ${doctorData?['lName'] ?? ''}';
+
+    return {
+      'doctorId': highestRatedDoctorId,
+      'doctorName': doctorName,
+      'rating': highestAvgRating
+    };
+  }
+
+  Widget _buildAppointmentsPerDayChart(Map<String, int> appointmentsPerDay) {
+    final List<FlSpot> spots = [];
+    final List<String> dateLabels = [];
+
+    DateTime now = DateTime.now();
+
+    for (int i = 2; i >= 0; i--) {
+      DateTime day = now.subtract(Duration(days: i));
+      String dayStr = DateFormat('yyyy-MM-dd').format(day);
+      int count = appointmentsPerDay[dayStr] ?? 0;
+      spots.add(FlSpot((2 - i).toDouble(), count.toDouble()));
+      dateLabels.add(DateFormat('dd MMM').format(day)); // Format as "dd MMM"
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text('Appointments Per Day (Last 3 Days)'),
+              const SizedBox(height: 8.0),
+              SizedBox(
+                height: 200,
+                child: LineChart(
+                  LineChartData(
+                    gridData: const FlGridData(show: true),
+                    titlesData: FlTitlesData(
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: (value, meta) {
+                            int index = value.toInt();
+                            if (index >= 0 && index < dateLabels.length) {
+                              return Text(
+                                dateLabels[index],
+                                style: const TextStyle(
+                                  fontSize: 10,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              );
+                            }
+                            return const Text('');
+                          },
+                        ),
+                      ),
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: (value, meta) {
+                            if (value % 2 == 0) {
+                              return Text(value.toInt().toString());
+                            }
+                            return const Text('');
+                          },
+                          reservedSize: 28,
+                        ),
+                      ),
+                    ),
+                    borderData: FlBorderData(show: true),
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: spots,
+                        isCurved: true,
+                        barWidth: 2,
+                        color: Colors.blue,
+                        belowBarData: BarAreaData(show: true),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
